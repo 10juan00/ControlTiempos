a691b79112efbefbddeea71225b77c7d6c4fdae6
RiverJu <10juan00@gmail.com>
2024-08-30 10:22:17 -0600

    Estructura entradas salidas terminado, Partes principales de Activities terminados y falta los fragments para determinar la forma en la que sepresenta la información.

--- app/src/main/java/com/example/controltiempos/ActivityTasks.kt
+++ app/src/main/java/com/example/controltiempos/ActivityTasks.kt
@@ -1,20 +1,34 @@
 package com.example.controltiempos
 
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ActivityTasks : AppCompatActivity() {
+    private lateinit var fab: FloatingActionButton
+    private lateinit var fragmentContainer: FrameLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tareas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_tasks)
+
+        fragmentContainer = findViewById(R.id.fragment_container)
+        fab = findViewById(R.id.fab)
+
+        fab.setOnClickListener {
+            fragmentContainer.visibility = View.VISIBLE
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FragmentIO())
+                .commit()
         }
     }
+
 }
